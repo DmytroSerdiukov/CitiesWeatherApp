0bb52dee2c89b2ce7116af5afcde11d720dac2e9
SDY <dmytro.serdiukov@gmail.com>
2022-12-13 17:30:12 +0200

    feature: api fetchCItyById

--- src/api/index.ts
+++ src/api/index.ts
@@ -13,6 +13,18 @@ export const CityAPI = {
     )
     return response.data
   },
+  fetchCityById: async (id: any) => {
+    const response = await axios.get<any>(
+      `https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?id=${id}&appid=665b5a3342ce18418ac2dc565903a417`,
+      {
+        headers: {
+          'Content-Type': 'aplication/json',
+          'Access-Allow-Control-Origin': '*'
+        }
+      }
+    )
+    return response.data
+  },
   fetchHourlyForecastForCity: async (city: string) => {
     const response = await axios.get<any>(
       `https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?q=${city}&exclude=hourly&appid=665b5a3342ce18418ac2dc565903a417`,
