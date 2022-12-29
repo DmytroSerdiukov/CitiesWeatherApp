f0a92ae52c11a8e6cb0c857a01bd9efbecc8fdbd
SDY <dmytro.serdiukov@gmail.com>
2022-12-29 17:18:56 +0200

    refactor: city card tests

--- src/__tests__/CityCard.test.tsx
+++ src/__tests__/CityCard.test.tsx
@@ -1,69 +1,66 @@
 import React from 'react'
 import { render, cleanup, getByTestId, fireEvent } from '@testing-library/react'
 import { MemoryRouter as Router } from 'react-router-dom'
+import { Provider } from 'react-redux'
 import { act } from 'react-dom/test-utils'
 import { data } from '../__mocks__/data'
 import { store } from '../store'
-import { Provider } from 'react-redux'
-import mockAxios from 'axios'
 import CityCard from '../components/CityCard/container'
 import CityCardMarkup from '../components/CityCard/markup'
+import mockAxios from 'axios'
 
 jest.mock('axios')
+beforeEach(() => {
+  mockAxios.get.mockResolvedValueOnce(data)
+})
 afterEach(cleanup)
 
-test('shows loader before card data fetched', async () => {
-  mockAxios.get.mockResolvedValueOnce(data)
-
-  const { findByRole } = render(
-    <Router>
-      <Provider store={store}>
-        <CityCard city={'Kyiv, UA'} />
-      </Provider>
-    </Router>
-  )
-  const loader = await findByRole('status')
-  expect(loader).toBeInTheDocument()
-})
-
-test('rendering card', async () => {
-  mockAxios.get.mockResolvedValueOnce(data)
-
-  const { findByText } = await act(() =>
-    render(
+describe('testing city card', () => {
+  const setup = () => {
+    return render(
       <Router>
         <Provider store={store}>
           <CityCard city={'Kyiv, UA'} />
         </Provider>
       </Router>
     )
-  )
-  const city = await findByText(/kyiv/i)
-  expect(city).toBeInTheDocument()
-})
-test('weather updates when update button hitted', async () => {
-  const deleteCityFromStorage = jest.fn()
-  const getCurrentWeather = jest.fn()
+  }
 
-  const { findByText, getByTestId } = await act(async () =>
-    render(
-      <Router>
-        <Provider store={store}>
-          <CityCardMarkup
-            getCurrentWeather={getCurrentWeather}
-            deleteCityFromStorage={deleteCityFromStorage}
-            data={data.data}
-            cityName={'Kyiv, UA'}
-          />
-        </Provider>
-      </Router>
+  test('shows loader before card data fetched', async () => {
+    const { findByRole } = setup()
+    const loader = await findByRole('status')
+    expect(loader).toBeInTheDocument()
+  })
+
+  test('rendering card', async () => {
+    const { findByText } = await act(() => setup())
+    const city = await findByText(/kyiv/i)
+    expect(city).toBeInTheDocument()
+  })
+  test('weather updates when update button hitted', async () => {
+    const deleteCityFromStorage = jest.fn()
+    const getCurrentWeather = jest.fn()
+
+    const { findByText, getByTestId } = await act(async () =>
+      render(
+        <Router>
+          <Provider store={store}>
+            <CityCardMarkup
+              getCurrentWeather={getCurrentWeather}
+              deleteCityFromStorage={deleteCityFromStorage}
+              data={data.data}
+              cityName={'Kyiv, UA'}
+            />
+          </Provider>
+        </Router>
+      )
     )
-  )
-  let city = await findByText(/kyiv/i)
-  expect(city).toBeInTheDocument()
-  const update = getByTestId('update')
-  fireEvent.click(update)
-  expect(getCurrentWeather).toHaveBeenCalled()
-  city = await findByText(/kyiv/i)
-  expect(city).toBeInTheDocument()
+    let city = await findByText(/kyiv/i)
+    expect(city).toBeInTheDocument()
+    const update = getByTestId('update')
+    fireEvent.click(update)
+    expect(getCurrentWeather).toHaveBeenCalled()
+    city = await findByText(/kyiv/i)
+    expect(city).toBeInTheDocument()
+  })
 })
