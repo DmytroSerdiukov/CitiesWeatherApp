11129a41e5d4921c9140bf867060023a08a71c11
SDY <dmytro.serdiukov@gmail.com>
2022-12-29 11:15:34 +0200

    feature: loader test for details page

--- src/__tests__/DetailsPage.test.tsx
+++ src/__tests__/DetailsPage.test.tsx
@@ -9,31 +9,45 @@ import { data } from '../__mocks__/data'
 jest.mock('axios')
 afterEach(cleanup)
 
-describe('Detais page testing', () => {
-  test('details data is fetched', async () => {
-    mockAxios.get.mockResolvedValueOnce(data)
-    await act(async () =>
-      render(
-        <MemoryRouter initialEntries={['/cities/703448']}>
-          <Routes>
-            <Route path='cities'>
-              <Route path=':id' element={<DetailsPage />} />
-            </Route>
-          </Routes>
-        </MemoryRouter>
-      )
+test('shows loader before data fetched', async () => {
+  mockAxios.get.mockResolvedValueOnce(data)
+
+  render(
+    <MemoryRouter initialEntries={['/cities/703448']}>
+      <Routes>
+        <Route path='cities'>
+          <Route path=':id' element={<DetailsPage />} />
+        </Route>
+      </Routes>
+    </MemoryRouter>
+  )
+  const loader = screen.getByRole('status')
+  expect(loader).toBeInTheDocument()
+})
+
+test('details data is fetched', async () => {
+  mockAxios.get.mockResolvedValueOnce(data)
+  await act(async () => {
+    render(
+      <MemoryRouter initialEntries={['/cities/703448']}>
+        <Routes>
+          <Route path='cities'>
+            <Route path=':id' element={<DetailsPage />} />
+          </Route>
+        </Routes>
+      </MemoryRouter>
     )
-
-    const cityName = await screen.findByText(/kyiv/i)
-    const descirption = await screen.findByText(/description/i)
-    const feelslike = await screen.findByText(/feels like/i)
-    const wind = await screen.findByText(/wind/i)
-    const pressure = await screen.findByText(/pressure/i)
-
-    expect(cityName).toBeInTheDocument()
-    expect(descirption).toBeInTheDocument()
-    expect(feelslike).toBeInTheDocument()
-    expect(wind).toBeInTheDocument()
-    expect(pressure).toBeInTheDocument()
   })
+
+  const cityName = await screen.findByText(/kyiv/i)
+  const descirption = await screen.findByText(/description/i)
+  const feelslike = await screen.findByText(/feels like/i)
+  const wind = await screen.findByText(/wind/i)
+  const pressure = await screen.findByText(/pressure/i)
+
+  expect(cityName).toBeInTheDocument()
+  expect(descirption).toBeInTheDocument()
+  expect(feelslike).toBeInTheDocument()
+  expect(wind).toBeInTheDocument()
+  expect(pressure).toBeInTheDocument()
 })
